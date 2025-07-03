deb7e623f99a78fd598fd89d92a336b0b2fdd491
H <caphe.2122006@gmail.com>
2025-07-03 15:02:30 +0700

    oneupmushroom block

---
+++ inc/Block/QuestionOneUpMushroomBlock.h
@@ -0,0 +1,25 @@
+#ifndef QUESTION_ONE_UP_MUSHROOM_BLOCK_H
+#define QUESTION_ONE_UP_MUSHROOM_BLOCK_H
+
+#include "Block/Block.h"
+#include "Game/Map.h"
+#include "Mario/Mario.h"
+
+class QuestionOneUpMushroomBlock : public Block {
+public:
+    QuestionOneUpMushroomBlock(Vector2 pos, Vector2 size, Color color);
+	QuestionOneUpMushroomBlock(Vector2 pos, Vector2 size, Color color, float frameTime, int maxFrames);
+    ~QuestionOneUpMushroomBlock();
+
+    void update() override;
+    void draw() override;
+    void doHit(Mario& mario, Map* map) override;
+
+private:
+    Item* item;
+	float itemVelocityY;
+	float itemMinY;
+	Map* map;
+};
+
+#endif // QUESTION_ONE_UP_MUSHROOM_BLOCK_H
