eb1779be63c44c8324e3fe3690860301a584c405
Đức Thiên Trần <thiencole16102006@gmail.com>
2025-07-18 14:06:02 +0700

    delete

--- build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tranducthien/Documents/Project/Mario-Project/raylib
-# Build directory: /Users/tranducthien/Documents/Project/Mario-Project/build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
